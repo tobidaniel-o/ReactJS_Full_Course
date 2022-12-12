22d24ad4d80368293c75a0a2f3734bc691edc1f9
Oluwatobi Okunuga <tobi@macbook-pro.home>
2022-12-11 20:33:02 -0500

    updated state to localStorage

--- src/App.js
+++ src/App.js
@@ -5,44 +5,40 @@ import { useState } from "react";
 import AddItem from "./AddItem";
 
 function App() {
-	const [items, setItems] = useState([
-		{
-			id: 1,
-			checked: true,
-			item: "One half pound bag of Cocoa Covered Almonds Unsalted",
-		},
-		{
-			id: 2,
-			checked: false,
-			item: "Item 2",
-		},
-		{
-			id: 3,
-			checked: false,
-			item: "Item 3",
-		},
-	]);
+	const [items, setItems] = useState(
+		JSON.parse(localStorage.getItem("shoppinglist"))
+	);
 
 	const [newItem, setNewItem] = useState("");
+
+	const setAndSaveItems = (newItems) => {
+		setItems(newItems);
+		localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+	};
+
+	const addItem = (item) => {
+		const id = items.length ? items[items.length - 1].id + 1 : 1;
+		const myNewItem = { id, checked: false, item };
+		const listItems = [...items, myNewItem];
+		setAndSaveItems(listItems);
+	};
 
 	const handleCheck = (id) => {
 		const listItems = items.map((item) => {
 			return item.id === id ? { ...item, checked: !item.checked } : item;
 		});
-		setItems(listItems);
-		localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+		setAndSaveItems(listItems);
 	};
 
 	const handleDelete = (id) => {
 		const listItems = items.filter((item) => item.id !== id);
-		setItems(listItems);
-		localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+		setAndSaveItems(listItems);
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		if (!newItem) return;
-		// addItem
+		addItem(newItem);
 		setNewItem("");
 	};
 	return (
